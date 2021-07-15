f549463c162d1a38d9f40fdc723e86cf45956303
mariaxo <72655567+mariaxo@users.noreply.github.com>
2021-07-15 21:09:36 +0400

    Initial commit

---
+++ ListImplementation/ListImplementation/List.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListImplementation
+{
+    public class List<T> : IList<T>, System.Collections.IList, IReadOnlyList<T>
+    {
+        const int MaxArrayLength = 0X7FEFFFFF;
+
+        private const int _defaultCapacity = 4;
+        private T[] _items;
+        private static readonly T[] _emptyArray = new T[0];
+        private int _size;
+        private int _version;
+        public int Count
+        {
+            get
+            {
+                //Contract code
+                return _size;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                //Contract code
+                return _items.Length;
+            }
+            set
+            {
+                if (value < _size)
+                {
+                    ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.value, ExceptionResource.ArgumentOutOfRange_SmallCapacity);
+                }
+                //Contract code
+                if (value != _items.Length)
+                {
+                    if (value > 0)
+                    {
+                        T[] newItems = new T[value];
+                        if (_size > 0)
+                        {
+                            Array.Copy(_items, 0, newItems, 0, _size);
+                        }
+
+                        _items = newItems;
+                    }
+                    else //when the capacity is < 0 or ==0
+                    {
+                        _items = _emptyArray;
+                    }
+                }
+            }
+        }
+
+        //constructors
+        public List()
+        {
+            _items = _emptyArray;
+        }
+
+        public List(int capacity)
+        {
+            if(capacity < 0 ) ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.capacity, ExceptionResource.ArgumentOutOfRange_NeedNonNegNum);
+            //Contract code
+            if (capacity == 0) _items = _emptyArray;
+            else _items = new T[capacity];
+        }
+
+        public List(IEnumerable<T> collection)
+        {
+            if (collection == null)
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.collection);
+            Contract.EndContractBlock();
+
+            ICollection<T> c = collection as ICollection<T>;
+            //if it's a collection, means <as> worked
+            if (c != null)
+            {
+                int count = c.Count;
+                if (count == 0)
+                {
+                    _items = _emptyArray;
+                }
+                else
+                {
+                    _items = new T[count];
+                    c.CopyTo(_items, 0);
+                    _size = count;
+                }
+            }
+            else
+            {
+                _size = 0;
+                _items = _emptyArray;
+                // This enumerable could be empty.  Let Add allocate a new array, if needed.
+                // Note it will also go to _defaultCapacity first, not 1, then 2, etc.
+
+                using (IEnumerator<T> en = collection.GetEnumerator())
+                {
+                    while (en.MoveNext())
+                    {
+                        Add(en.Current);
+                    }
+                }
+            }
+        }
+
+        //methods
+        public void Add(T item)
+        {
+            if (_size == _items.Length)
+            {
+                EnsureCapacity(_size + 1);
+            }
+            _items[_size] = item;
+            _size++;
+            //modification of the list so detection needed
+            _version++;
+        }
+        public int Add(object item)
+        {
+            //checking if item is null??
+            ThrowHelper.IfNullAndNullsAreIllegalThenThrow<T>(item, ExceptionArgument.item);
+            try
+            {
+                Add((T)item);
+            }
+            catch (InvalidCastException)
+            {
+                ThrowHelper.ThrowWrongValueTypeArgumentException(item, typeof(T));
+            }
+            //this method shall return the index of the newly added item
+            return Count - 1;
+        }
+
+        private void EnsureCapacity(int min)
+        {
+            if (_items.Length < min)
+            {
+                int newCapacity = _items.Length == 0 ? _defaultCapacity : _items.Length * 2;
+                //the list might grow and pass the MaxArrayLength, so a check is needed
+                if ((uint)newCapacity > MaxArrayLength) newCapacity = MaxArrayLength;
+               
+                //when doubling the capacity, still we can get less than the min, so we ensure it's at least the min
+                if (newCapacity < min) newCapacity = min;
+                Capacity = newCapacity;
+            }
+
+
+        }
+
+        public void Clear()
+        {
+            if (_size > 0)
+            {
+                Array.Clear(_items, 0, _size);
+                _size = 0;
+            }
+            //as there is a modification, we need to detect it
+            _version++;
+        }
+
+    }
+}
